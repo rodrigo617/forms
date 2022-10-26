689100f1580cff2cf69c976a543a08261f7db47a
Rodrigo617 <ingenieria.rlopez@gmail.com>
2022-10-26 11:25:59 -0300

    I finished the review of forms in React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import { useRef } from "react"
+//en este ejemplo estoy guardando el estado de un campo de un 
+//formmulario guardando dentro del estado de useState.
+//pero si quiero guardar mas valores??
+// >>>> puedo guardar (en lugar de un '') un objeto literal.
+
+import { Form } from 'formik'
+import {useState } from 'react'
 
 const App = () => {
-  const input = useRef()
-  const file = useRef()
-  const submit = () => {
-    console.log(input.current.value);
-    console.log(file.current.files[0])
-    const form = new FormData()
-    form.append('archivo', file.current.files[0])
-    form.append('campo', input.current.value)
-    fetch('/lala', {method: 'POST', body: form})
+  //const [value, setValue] = useState('')
+  const [value, setValue] = useState({ normal: '', texto: '', select: 'valor 1', check: false})
+  const handleChange = ({ target }) => {
+ 
+    setValue((state) => ({ 
+      ...state,
+      [target.name]: target.type === 'checkbox' 
+      ? target.checked 
+      : target.value 
+    }))
+
   }
+  console.log(value);
+  
   return (
     <div>
-      <div>
-        <span>Lala</span>
-        <input type='text' name= 'campo' ref={input}/>
-        <input type='file' ref={file}/>
-      </div>
-      <input type='submit' value= 'Enviar' onClick={submit} />
+      {value.length < 5 ? <span>longitud minima de 5</span>: null}
+      <input type="text" name="normal" value={value.normal} onChange={handleChange}/>
+      <textarea name='texto' onChange={handleChange} value={value.texto} />
+    
+    <select value={value.select} name='select' onChange={{handleChange}}>
+    <option value=''>-- seleccione --</option>
+    <option value='valor 1'>Valor 1</option>
+    <option value='valor 2'>Valor 2</option>
+    <option value='valor 3'>Valor 3</option>
 
+    </select>
+
+    <input 
+      type="checkbox"
+      name="check"
+      onChange={handleChange}
+      checked={Form.check}
+    
+    />
+
+    <div>
+      <label>Seleccione</label>
+      <input 
+        onChange={handleChange} 
+        type="radio" 
+        value="Op 1" 
+        name="estado" 
+        checked={value.estado === 'Op 1'}
+      /> Opcion 1
+      <input 
+        onChange={handleChange} 
+        type="radio" 
+        value="Op 2" 
+        name="estado" 
+        checked={value.estado === 'Op 2'}
+      /> Opcion 2
+      <input 
+        onChange={handleChange} 
+        type="radio" 
+        value="Op 3" 
+        name="estado" 
+        checked={value.estado === 'Op 3'}
+      /> Opcion 3
     </div>
+    
+    </div>
+
   )
 }
 
